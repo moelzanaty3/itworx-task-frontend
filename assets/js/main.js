e675c0e8b237ea4ff051a5236cd3b000ca596b47
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2018-11-06 20:58:07 +0200

    finish servy

--- assets/js/main.js
+++ assets/js/main.js
@@ -416,3 +416,38 @@ var args = {
 };
 
 $(".carousel").MZNZN(args);
+
+(function($) {
+  // tabbed content
+  $(".tab_content").hide();
+  $(".tab_content:first").show();
+
+  /* if in tab mode */
+  $("ul.tabs li").click(function() {
+    $(".tab_content").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).fadeIn();
+
+    $("ul.tabs li").removeClass("active");
+    $(this).addClass("active");
+
+    $(".tab_drawer_heading").removeClass("d_active");
+    $(".tab_drawer_heading[rel^='" + activeTab + "']").addClass("d_active");
+  });
+  /* if in drawer mode */
+  $(".tab_drawer_heading").click(function() {
+    $(".tab_content").hide();
+    var d_activeTab = $(this).attr("rel");
+    $("#" + d_activeTab).fadeIn();
+
+    $(".tab_drawer_heading").removeClass("d_active");
+    $(this).addClass("d_active");
+
+    $("ul.tabs li").removeClass("active");
+    $("ul.tabs li[rel^='" + d_activeTab + "']").addClass("active");
+  });
+
+  $("ul.tabs li")
+    .last()
+    .addClass("tab_last");
+})(jQuery);
